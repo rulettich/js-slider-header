ce258cf90e61d45710a9f8d518de6acbb8e578be
rulettich <rulettich@gmail.com>
2024-07-09 16:04:15 +0300

    Home task To-do-list

--- src/route/index.js
+++ src/route/index.js
@@ -6,12 +6,15 @@ const router = express.Router()
 const test = require('./test')
 const calc = require('./calc')
 const community = require('./community')
+
+const toDoList = require('./toDoList')
 // Підключіть інші файли роутів, якщо є
 
 // Об'єднайте файли роутів за потреби
 router.use('/', test)
 router.use('/calc', calc)
 router.use('/community', community)
+router.use('/toDoList', toDoList)
 // Використовуйте інші файли роутів, якщо є
 
 // Експортуємо глобальний роутер
